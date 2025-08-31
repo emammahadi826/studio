7b73c83952cce73214e3105dcafd9fc86b2cbc83
Mahadi <usefordokan@gmail.com>
2025-08-31 16:52:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <SidebarProvider>
-            <MainSidebar />
+            {isMounted && <MainSidebar />}
             <SidebarInset>
                 <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 md:hidden">
                    {isMounted && <SidebarTrigger />}
