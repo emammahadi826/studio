acd91f9155f47f2ab8ea7dacc4618eedc6451f01
Mahadi <usefordokan@gmail.com>
2025-09-01 07:52:24 +0000

    that show a scroll show section can you remove thats

--- src/components/notepad-view.tsx
+++ src/components/notepad-view.tsx
@@ -10,8 +10,8 @@ interface NotepadViewProps {
 
 export function NotepadView({ content, onContentChange }: NotepadViewProps) {
   return (
-    <div className="w-full h-full p-4 md:p-8">
-      <div className="w-full h-full max-w-4xl mx-auto">
+    <div className="w-full h-full">
+      <div className="w-full h-full max-w-4xl mx-auto p-4 md:p-8">
         <Editor
             content={content}
             onUpdate={({ editor }) => {
